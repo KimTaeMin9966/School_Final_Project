9580529bb825a10b2dc981c389c9c68e75aa38f1
User <User@User-PC>
2018-10-24 15:46:19 +0900

    2018/10/24 AAA

--- final_Project/src/main/java/net/koreate/controller/WeddingController.java
+++ final_Project/src/main/java/net/koreate/controller/WeddingController.java
@@ -6,5 +6,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/wedding/*")
 public class WeddingController {
+	@RequestMapping(value="/send")
+	public void send() {
+		System.out.println("send접속완료");
+	}
 	
 }
+
+
