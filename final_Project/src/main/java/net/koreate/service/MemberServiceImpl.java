842ca04b0bb82d79cf478266f1c165c81461e571
User <User@User-PC>
2018-10-30 16:50:11 +0900

    UP

--- final_Project/src/main/java/net/koreate/service/MemberServiceImpl.java
+++ final_Project/src/main/java/net/koreate/service/MemberServiceImpl.java
@@ -3,7 +3,6 @@ package net.koreate.service;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import net.koreate.dao.MemberDao;
 import net.koreate.dto.LoginDto;
