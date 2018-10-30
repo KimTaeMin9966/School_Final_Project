6bbdf24cc50d818dc99111ed16fcd1330b068013
User <User@User-PC>
2018-10-30 14:51:22 +0900

    up

--- final_Project/src/main/webapp/resources/js/custom.js
+++ final_Project/src/main/webapp/resources/js/custom.js
@@ -13,7 +13,7 @@
 		
 		var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 		var options = { //지도를 생성할 때 필요한 기본 옵션
-			center: new daum.maps.LatLng(35.2075826,129.0701494), //지도의 중심좌표.
+			center: new daum.maps.LatLng('${}','${}'), //지도의 중심좌표.
 			level: 3 //지도의 레벨(확대, 축소 정도)
 		};
 
