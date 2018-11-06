a1fdeaa912632c37c4a4060bca943a9c0c70c290
User <User@User-PC>
2018-11-06 14:41:21 +0900

    UPDATE K

--- final_Project/src/main/java/net/koreate/controller/hall/WeddingHallController.java
+++ final_Project/src/main/java/net/koreate/controller/hall/WeddingHallController.java
@@ -3,11 +3,11 @@ package net.koreate.controller.hall;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -38,52 +38,51 @@ public class WeddingHallController {
 	}*/
 	
 	@RequestMapping(value = "/area1", method = RequestMethod.GET)
-	public void area1GET(WeddingHallVo vo, Model model) throws Exception {
+	public void area1GET(WeddingHallVo vo, HttpSession session) throws Exception {
 		logger.info("area1GET Called!!!(부산진구)");
 		vo.setHall_area(1);
 		List<WeddingHallVo> list = service.SearchArea(vo);
-		model.addAttribute("list", list);
+		session.setAttribute("list", list);
 	}
 	
 	@RequestMapping(value = "/area2", method = RequestMethod.GET)
-	public void area2GET(WeddingHallVo vo, Model model) throws Exception {
+	public void area2GET(WeddingHallVo vo, HttpSession session) throws Exception {
 		logger.info("area2GET Called!!!");
 		vo.setHall_area(2);
 		List<WeddingHallVo> list = service.SearchArea(vo);
-		model.addAttribute("list", list);
-		
+		session.setAttribute("list", list);
 	}
 	
 	@RequestMapping(value = "/area3", method = RequestMethod.GET)
-	public void area3GET(WeddingHallVo vo, Model model) throws Exception {
+	public void area3GET(WeddingHallVo vo, HttpSession session) throws Exception {
 		logger.info("area3GET Called!!!");
 		vo.setHall_area(3);
 		List<WeddingHallVo> list = service.SearchArea(vo);
-		model.addAttribute("list", list);
+		session.setAttribute("list", list);
 	}
 	
 	@RequestMapping(value = "/area4", method = RequestMethod.GET)
-	public void area4GET(WeddingHallVo vo, Model model) throws Exception {
+	public void area4GET(WeddingHallVo vo, HttpSession session) throws Exception {
 		logger.info("area4GET Called!!!");
 		vo.setHall_area(4);
 		List<WeddingHallVo> list = service.SearchArea(vo);
-		model.addAttribute("list", list);
+		session.setAttribute("list", list);
 	}
 	
 	@RequestMapping(value = "/area5", method = RequestMethod.GET)
-	public void area5GET(WeddingHallVo vo, Model model) throws Exception {
+	public void area5GET(WeddingHallVo vo, HttpSession session) throws Exception {
 		logger.info("area5GET Called!!!");
 		vo.setHall_area(5);
 		List<WeddingHallVo> list = service.SearchArea(vo);
-		model.addAttribute("list", list);
+		session.setAttribute("list", list);
 	}
 	
 	@RequestMapping(value = "/area6", method = RequestMethod.GET)
-	public void area6GET(WeddingHallVo vo, Model model) throws Exception {
+	public void area6GET(WeddingHallVo vo, HttpSession session) throws Exception {
 		logger.info("area6GET Called!!!");
 		vo.setHall_area(6);
 		List<WeddingHallVo> list = service.SearchArea(vo);
-		model.addAttribute("list", list);
+		session.setAttribute("list", list);
 	}
 	
 }
